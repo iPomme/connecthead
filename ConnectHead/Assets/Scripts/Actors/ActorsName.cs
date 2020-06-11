d9b5600abf6c4baaa893a82e999214e462e744ce
Nicolas Jorand <nicolas.jorand@me.com>
2020-06-12 00:12:57 +0200

    Add the dev framework

---
+++ ConnectHead/Assets/Scripts/Actors/ActorsName.cs
@@ -0,0 +1,7 @@
+namespace Actors
+{
+    public enum ActorsName
+    {
+        UiManager
+    }
+}
---
+++ ConnectHead/Assets/Scripts/UI/UiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Actors;
+using Onorno.ProtoActor;
+using Proto;
+
+namespace UI
+{
+    public class UiController : MonoBehaviourWithLoggerWithProtoActor
+    {
+
+
+        public override string ActorName => nameof(ActorsName.UiManager);
+        public override Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                default:
+                    Logger.WarnFormat("Received unknown message: {0}", context.Message.ToString());
+                    break;
+            }
+
+            return Actor.Done;
+        }
+    }
+}
