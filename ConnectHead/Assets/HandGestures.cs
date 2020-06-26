617c8abf82f2c655b00b4c193f5b180e4b65e785
oenvoyage <oenvoyage@gmail.com>
2020-06-26 21:11:50 +0200

    more research on hand tracking gestures (WIP)

---
+++ ConnectHead/Assets/HandGestures.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGestures : MonoBehaviour
+{
+    public Collider colliderThumb;
+    public Collider colliderIndex;
+    public Collider colliderMiddle;
+    public Collider colliderPinky;
+
+    public Collider colliderInsidePalm;
+    public Collider colliderStopSign;
+
+    public TextMesh confidenceTxt;
+    public TextMesh trackingStatus;
+    public TextMesh poseTxt;
+
+    private OVRHand rightHand;
+
+    private string detectedPose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightHand = GetComponent<OVRHand>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        detectedPose = "none";
+
+        trackingStatus.text = "tracking status = " + rightHand.IsTracked.ToString();
+        confidenceTxt.text = "confidence = " + rightHand.HandConfidence.ToString();
+        
+        poseTxt.text = "Pose = " + detectedPose;
+
+    }
+}
